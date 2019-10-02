bcea25f0ce2f65e553593559de548fb00e977336
miguel5612 <miguelangelcu@ufps.edu.co>
2019-10-01 21:58:48 -0500

    Project ended

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,9 +1,18 @@
-int pulse = 60; //led 1 second 1, 1 second off
+int pulse = 600; //led 1 second 1, 1 second off
+int tmpFrq = 0;
+int frecuencia = 0;
+
 int pulCounted = 0;
 const int LEDPin = 13;
 const int btnUp = 3;
 const int btnDown = 4;
 const int ZCPin = 2;
+
+unsigned long lastMeasure = 0;
+unsigned long setupTime = 0;
+unsigned long flagTime = 0;
+
+bool pulsosPorSegundo = false;
 
 void setup(){
     //PIn setup
@@ -23,8 +32,11 @@ void loop(){
     //Program condition
     if(pulCounted > pulse)
     {
+        if(flagTime == 0) flagTime = millis();
         digitalWrite(LEDPin, HIGH);
+        Serial.println("Por segundo son: " + String(frecuencia) + " Pulsos");
         Serial.println("He contado: " + String(pulCounted) + " Pulsos");
+        Serial.println("Han pasado " + String((flagTime-setupTime)) + " milisegundos antes de llegar al limite impuesto");
     }
     else
     {
@@ -47,5 +59,12 @@ void loop(){
 }
 
 void detectarPulsos(){ 
+  if(millis()-lastMeasure>1000){
+      lastMeasure = millis();
+      frecuencia = tmpFrq;
+      tmpFrq = 0;
+  }
+  if(setupTime == 0) setupTime =  millis();
   pulCounted++;
+  tmpFrq++;
 }
